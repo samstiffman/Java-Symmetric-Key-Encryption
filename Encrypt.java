5a2cb33f15db0d19e0c4a9bf3040ef62ed22e253
Sam Stiffman <samstiffman@gmail.com>
2018-05-11 13:12:12 -0500

    Add files via upload

---
+++ Encrypt.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Encrypt {
+
+	public static byte[] encrypt(byte[] originalFile, SymmetricKey key) {
+		
+		
+		ArrayList<Byte> fileList = new ArrayList<>(originalFile.length);  
+		for(byte e: originalFile)
+			fileList.add(e);
+		final double size = fileList.size();
+		for(int i=0; i<size;) {
+			try {
+				for(int q=0; q<key.hashValue.length;q++) {
+					fileList.set(i, (byte)(fileList.get(i)^key.hashValue[q]));
+					i++;
+				}
+			}catch(IndexOutOfBoundsException e) {
+				break;
+			}
+		}
+		byte[] returnArray = new byte[(int)size];
+		for(int i=0; i<fileList.size(); i++) {
+			returnArray[i] = fileList.get(i);
+		}
+		return returnArray;
+	}
+	public static byte[] decrypt(ArrayList<Byte> allBytes, Key test) {
+				
+		byte[] returnArray = new byte[1];
+		return returnArray ;
+	}
+	
+	
+	
+	
+}
